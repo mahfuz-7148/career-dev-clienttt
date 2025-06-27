3356b2bf4fde06638d527df69cffac8dec030612
mahfuz-7148 <mrahman7148@gmail.com>
2025-06-27 20:17:31 +0600

    first commit

---
+++ src/Components/ViewApplication.jsx
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import {useParams} from 'react-router';
+import {useQuery} from '@tanstack/react-query';
+
+
+const applicationDetails = async id => {
+    const response = await axios.get(`${import.meta.env.VITE_SERVER}/applications/job/${id}`)
+    return response.data
+}
+
+const ViewApplication = () => {
+
+    const {id} = useParams()
+    const {data: jobId} = useQuery({
+        queryKey: ['jobId', id],
+        queryFn: () => applicationDetails(id)
+    })
+    console.log(jobId)
+    const statusChange = (e, app_id) => {
+        console.log(e.target.value, app_id)
+
+        axios.patch(`${import.meta.env.VITE_SERVER}/applications/${app_id}`, {
+            status: e.target.value
+        })
+            .then(res => res.data)
+            .catch(error => console.log(error)
+            )
+
+    }
+
+
+    return (
+        <div>
+            <h2 className="text-4xl">job view applications {jobId?.length}</h2>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Job</th>
+                        <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        jobId?.map(application =>
+                            <tr key={application._id}>
+                                <th>1</th>
+                                <td>{application.applicant}</td>
+                                <td>Quality Control</td>
+                                <td>
+                                    <select onChange={e => statusChange(e, application._id)} defaultValue={application.status}>
+                                        <option disabled={true}>Update Status</option>
+                                        <option>Pending</option>
+                                        <option>Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default ViewApplication;
---
+++ src/Contexts/AuthProvider.jsx
@@ -0,0 +1,65 @@
+import {createContext, useEffect, useState} from 'react';
+import {GoogleAuthProvider,
+        signInWithPopup,
+    signOut,
+    onAuthStateChanged
+} from 'firebase/auth'
+import {auth} from '../firebase.js';
+import axios from 'axios';
+
+
+export const AuthCotext = createContext(null)
+const goggleProvider = new GoogleAuthProvider()
+
+const AuthProvider = ({children}) => {
+    const [loading, setLoading] = useState(true)
+    const [saveUser, setSaveUser] = useState(null)
+
+    const loginGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, goggleProvider)
+
+    }
+
+    const signOutUser = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setSaveUser(currentUser)
+            setLoading(false)
+            if (currentUser?.email) {
+                axios.post('http://localhost:3000/jwt', {
+                    email: currentUser.email
+                },{
+                    withCredentials: true
+                })
+                    .then(res => console.log(res.data))
+                    .catch(error => console.log(error)
+                    )
+            }
+            console.log(currentUser)
+
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, []);
+
+
+    const userInfo = {
+        loginGoogle,
+        signOutUser,
+        loading,
+        saveUser
+    }
+    return (
+        <AuthCotext value={userInfo}>
+            {children}
+        </AuthCotext>
+    );
+};
+
+export default AuthProvider;
